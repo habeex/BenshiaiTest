5f84e7eef782119671121d8a789c0c2c792ac8e4
olorunisholah abeeb <olorunisholahabeeb@gmail.com>
2022-08-24 19:51:26 +0100

    Remove personal api key

--- app/src/main/java/ai/benshi/test/utils/Constants.kt
+++ app/src/main/java/ai/benshi/test/utils/Constants.kt
@@ -6,6 +6,6 @@ class Constants {
         const val IMAGE_BASE_URL = "https://picsum.photos/seed"
         const val STATIC_MAP = "https://maps.googleapis.com/maps/api/staticmap?"
         ///TODO google api key is required to get staticmap
-        const val API_KEY = ""
+        const val API_KEY = "" //This is here just for sake of testing
     }
 }
